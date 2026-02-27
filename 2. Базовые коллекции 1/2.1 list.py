40bcbf46670a7f229bee12ff60cb31a8835a9cf6
kyrag <kyraginartem@gmail.com>
2026-02-27 14:28:31 +0700

    create 2.2 index

--- 2. Базовые коллекции 1/2.1 list.py
+++ 2. Базовые коллекции 1/2.1 list.py
@@ -28,14 +28,31 @@
 
 #########################
 
-numbers = [3,7,5]
+# numbers = [3,7,5]
+#
+# while True:
+#     number = int(input('Новое число: '))
+#     numbers.append(number)
+#
+#     print('Текущий список чисел: ', numbers)
+#
+#     for i in numbers:
+#         print( i ** 2, i ** 3, i ** 4)
+#     print()
 
-while True:
-    number = int(input('Новое число: '))
-    numbers.append(number)
+#################################
 
-    print('Текущий список чисел: ', numbers)
+# Задача 2. Очень простая задача
+#
+# У вас есть список numbers. Напишите программу, которая заполняет список числами от 0 до 100 и выводит его на экран.
+#
+#
+#
+# numbers = []
+# for number in range(101):
+#     numbers.append(number)
+# print(numbers)
 
-    for i in numbers:
-        print( i ** 2, i ** 3, i ** 4)
-    print()
+
+
+
